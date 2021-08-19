b283db9132a30d5d841c772167a49b61c926618e
BuildTools <unconfigured@null.spigotmc.org>
2021-08-19 21:44:51 +0530

    graph

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This is a python program which shows you the graph of wavelength versus energy d
 Planck's law describes the energy density of electromagnetic radiation emitted by a black body in thermal equilibrium at a given temperature T, when there is no net flow of matter or energy between the body and its environment.
 
 Every physical body spontaneously and continuously emits electromagnetic radiation and the energy density of a body, U, describes the energy per unit area, for particular radiation wavelengths. The relationship given by Planck's radiation law, given below, shows that with increasing temperature, the total radiated energy of a body increases and the peak of the emitted spectrum shifts to shorter wavelengths.[2] According to this, the energy density of a body for wavelngth lambda at absolute temperature T is given by
+
 ![this formula](res/readme/img.png)
 
 ## Built With
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ fig = plt.figure()
 
 axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
-wavelength_x = np.linspace(0.01, 1.6, 100) * (10 ** -6)
+wavelength_x = np.linspace(0.1, 1.6, 100) * (10 ** -6)
 
 temperature_range = range(3000, 6001, 1000)
 colors = "r g b y".split()
@@ -18,13 +18,19 @@ k = 1.38*(10**-23)
 count = 0
 for temp in temperature_range:
     energy_density_y = ((8*3.14*h*c)/(wavelength_x**5))*(1/(e**(h*c/(wavelength_x*k*temp))-1))
-    axes.plot(wavelength_x, energy_density_y, color=colors[count], label=str(temp) )
+    axes.plot(wavelength_x, energy_density_y, color=colors[count], label=str(temp), lw=3)
     count += 1
-# axes.plot(wavelength_x,spectral_radiance_y_2,"g-")
-# axes.plot(wavelength_x,spectral_radiance_y_3,"b-")
 
-axes.set_xlabel("Wavelength")
-axes.set_ylabel("Spectral Radiance")
+axes.plot(0.38 * (10 ** -6), 0, color="black", label="Visible Spectrum", lw=1, alpha=0.5)
+axes.plot(0.7 * (10 ** -6), 0, color="black", lw=1, alpha=0.5)
+
+axes.set_xlabel("Wavelength λ [m]")
+axes.set_ylabel("Energy Density u(λ) [J m-4]")
 axes.set_title("Black Body Radiation Spectrum")
+
+axes.set_xlim(0.1 * (10 ** -6), 1.6 * (10 ** -6))
+axes.set_ylim(0, 1500000)
+
+axes.legend()
 
 plt.show()
